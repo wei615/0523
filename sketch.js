841d3d3135fafcc82a88c24f53bc5df62a13e134
wei615 <413730150@o365.tku.edu.tw>
2025-05-23 10:28:36 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,48 +1,52 @@
 let facemesh;
-let predictions = [];
-const keypointsIdx = [409,270,269,267,0,37,39,40,185,61,146,91,181,84,17,314,405,321,375,291];
 let video;
+let predictions = [];
+// 指定要連接的facemesh點位編號
+const indices = [409,270,269,267,0,37,39,40,185,61,146,91,181,84,17,314,405,321,375,291];
 
 function setup() {
-  // 建立640x480畫布並置中
+  // 將畫布置中
   let cnv = createCanvas(640, 480);
+  cnv.style('display', 'block');
+  cnv.parent(document.body);
+  // 置中畫布
   cnv.position((windowWidth - width) / 2, (windowHeight - height) / 2);
 
-  // 啟用視訊
   video = createCapture(VIDEO);
   video.size(width, height);
   video.hide();
 
-  // 載入facemesh模型
   facemesh = ml5.facemesh(video, modelReady);
-  facemesh.on("predict", results => {
-    predictions = results;
-  });
+  facemesh.on('predict', gotResults);
 }
 
 function modelReady() {
-  console.log("Facemesh model loaded!");
+  // 模型載入完成
+}
+
+function gotResults(results) {
+  predictions = results;
 }
 
 function draw() {
   background(220);
+  image(video, 0, 0, width, height);
 
-  // 畫出facemesh關鍵點連線
-  drawFaceMeshLines();
+  drawFacemeshLines();
 }
 
-function drawFaceMeshLines() {
+function drawFacemeshLines() {
   if (predictions.length > 0) {
     const keypoints = predictions[0].scaledMesh;
     stroke('#ff006e');
     strokeWeight(15);
     noFill();
-    for (let i = 0; i < keypointsIdx.length - 1; i++) {
-      let idxA = keypointsIdx[i];
-      let idxB = keypointsIdx[i + 1];
-      let [x1, y1] = keypoints[idxA];
-      let [x2, y2] = keypoints[idxB];
-      line(x1, y1, x2, y2);
+    beginShape();
+    for (let i = 0; i < indices.length; i++) {
+      const idx = indices[i];
+      const [x, y] = keypoints[idx];
+      vertex(x, y);
     }
+    endShape();
   }
 }
